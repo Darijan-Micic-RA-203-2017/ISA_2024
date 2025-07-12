d94d9906128f81d8c0cf6447d72dfdf98c78aa90
Darijan Mićić <darijan.micic10@gmail.com>
2025-07-12 03:24:59 +0200

    fix: call correct cnstr. in subclasses of "user"

--- OnlyBunsBackend/src/main/java/ftn/project/OnlyBunsBackend/model/user/BasicUser.java
+++ OnlyBunsBackend/src/main/java/ftn/project/OnlyBunsBackend/model/user/BasicUser.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import ftn.project.OnlyBunsBackend.model.address.Address;
+import ftn.project.OnlyBunsBackend.model.chat.Chat;
+import ftn.project.OnlyBunsBackend.model.post.Post;
 
 @Entity
 @Table(name = "basic_users")
@@ -16,8 +18,11 @@ public class BasicUser extends User {
 
 	public BasicUser(long id, boolean isEnabled, Set<UserRole> roles, String username, 
 			String password, String emailAddress, String firstName, String lastName, 
-			Address residenceAddress) {
+			Address residenceAddress, Set<User> followedUsers, long numberOfFollowedUsers, 
+			Set<User> followingUsers, long numberOfFollowingUsers, Set<Post> createdPosts, 
+			Set<Post> likedPosts, Set<Chat> chats) {
 		super(id, isEnabled, roles, username, password, emailAddress, firstName, lastName, 
-				residenceAddress);
+				residenceAddress, followedUsers, numberOfFollowedUsers, followingUsers, 
+				numberOfFollowingUsers, createdPosts, likedPosts, chats);
 	}
 }
--- OnlyBunsBackend/src/main/java/ftn/project/OnlyBunsBackend/model/user/SystemAdministrator.java
+++ OnlyBunsBackend/src/main/java/ftn/project/OnlyBunsBackend/model/user/SystemAdministrator.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import ftn.project.OnlyBunsBackend.model.address.Address;
+import ftn.project.OnlyBunsBackend.model.chat.Chat;
+import ftn.project.OnlyBunsBackend.model.post.Post;
 
 @Entity
 @Table(name = "system_administrators")
@@ -16,8 +18,11 @@ public class SystemAdministrator extends User {
 
 	public SystemAdministrator(long id, boolean isEnabled, Set<UserRole> roles, String username, 
 			String password, String emailAddress, String firstName, String lastName, 
-			Address residenceAddress) {
+			Address residenceAddress, Set<User> followedUsers, long numberOfFollowedUsers, 
+			Set<User> followingUsers, long numberOfFollowingUsers, Set<Post> createdPosts, 
+			Set<Post> likedPosts, Set<Chat> chats) {
 		super(id, isEnabled, roles, username, password, emailAddress, firstName, lastName, 
-				residenceAddress);
+				residenceAddress, followedUsers, numberOfFollowedUsers, followingUsers, 
+				numberOfFollowingUsers, createdPosts, likedPosts, chats);
 	}
 }
